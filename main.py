62161794edf99cf397a6b1b3e0b78dbee382c916
KSD MSI PC <nhanksd85@gmail.com>
2022-05-16 15:52:57 +0700

    Init uart connection

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+import serial.tools.list_ports
+import time
+
 print("DCAR PRESIDENT")
+
+uart_port = ""
+if len(uart_port) > 0:
+    ser = serial.Serial(port=uart_port, baudrate=115200)
+    
+def processData(data):
+    data = data.replace("!", "")
+    data = data.replace("#", "")
+    splitData = data.split(":")
+    print(splitData)
+
+def readSerial():
+    bytesToRead = ser.inWaiting()
+    if (bytesToRead > 0):
+        global mess
+        mess = mess + ser.read(bytesToRead).decode("UTF-8")
+        while ("#" in mess) and ("!" in mess):
+            start = mess.find("!")
+            end = mess.find("#")
+            processData(mess[start:end + 1])
+            if (end == len(mess)):
+                mess = ""
+            else:
+                mess = mess[end+1:]
+while True:
+
+    if len(uart_port) >  0:
+        readSerial()
+    time.sleep(0.1)
